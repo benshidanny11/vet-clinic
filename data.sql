718d9a9251ef265c8611ebb40a16d44e1dee4b52
benshidanny11 <benshidanny11@gmail.com>
2022-09-19 18:47:40 +0200

    Implement performance audit

--- data.sql
+++ data.sql
@@ -1,54 +1,67 @@
-/* Populate database with sample data. */
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (1,'Agumon', '2020-02-03', 0, TRUE, '10.23');
 
-INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg)
-values('Agumon','2020-02-03',0,true,10.23),
-('Gabumon','2018-11-15',3,true,8),
-('Pikachu','2021-01-07',1,false,15.04),
-('Devimon','2017-05-12',5,true,11);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (2,'Gabumon', '2018-11-15', 2, TRUE, '8.0');
 
- INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) 
- VALUES('Charmander','2020-02-08',0,false,-11),
- ('Plantmon','2021-11-15',2,true,-5.7),
- ('Squirtle','1993-04-02',3,false,-12.13),
- ('Angemon','2005-06-12',1,true,-45),
- ('Boarmon','2005-06-07',7,true,20.4),
- ('Blossom','1998-10-13',3,true,17),
- ('Ditto','2022-05-14',4,true,22);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3,'Pikachu', '2021-01-07', 1, FALSE, '15.04');
 
-  INSERT INTO owners VALUES('Sam Smith',34),
-  ('Jennifer Orwell',19),
-  ('Bob',45),
-  ('Melody Pond',77),
-  ('Dean Winchester',14),
-  ('Jodie Whittaker',38);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (4,'Devimon', '2017-05-12', 5, TRUE, '11.0');
 
-  INSERT INTO species(name)
-   VALUES('Pokemon'),
-   ('Digimon');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (5, 'Devimon', '2017-05-12', 5, TRUE, '11.0');
 
-UPDATE animals SET species='digimon' WHERE name like '%mon';
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (6, 'Charmander', '2020-02-08', 0, FALSE, '-11.0');
 
-UPDATE animals SET species='pokemon' WHERE species='';
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (7, 'Plantmon', '2021-11-15', 2, TRUE, '-5.7');
 
- UPDATE animals SET weight_kg=weight_kg*-1 where weight_kg<0;
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (8, 'Squirtle', '1993-04-02', 3, TRUE, '-12.13');
 
- UPDATE animals SET weight_kg=weight_kg*-1;
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (9, 'Angemon', '2005-06-12', 1, TRUE, '-45.0');
 
-UPDATE animals SET species_id=2 WHERE name like '%mon';
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (10, 'Boarmon', '2005-06-7', 7, TRUE, '20.4');
 
-UPDATE animals SET species_id=1 WHERE name not like '%mon';
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (11, 'Blossom', '1998-10-13', 3, TRUE, '17.0');
 
-INSERT INTO vets(name,age,date_of_graduation) 
-VALUES('Vet William Tatcher',54,'2000-04-23'),
-('Vet Maisy Smith',26,'2019-06-17'),
-('Vet Stephanie Mendez',64,'1981-05-04'),
-('Vet Jack Harkness',38,'2008-06-08');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (12, 'Ditto', '2022-05-14', 4, TRUE, '22.0');
 
-INSERT INTO specializations(species_id,vets_id) VALUES(1,1),(1,3),(2,3),(2,4);
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
 
-INSERT INTO visits(animals_id,vets_id,date_of_visit) VALUES(1,1,'2020-05-23'),(1,3,'2020-07-22'),(2,4,'2021-02-02'),(3,2,'2020-01-05'),(3,2,'2020-05-08'),(3,2,'2020-05-14'),(4,3,'2021-05-04'),(5,4,'2021-02-24'),(6,2,'2019-12-21'),(6,1,'2020-08-10'),(6,2,'2021-04-07'),(7,3,'2019-09-29'),(8,4,'2020-10-03'),(8,4,'2020-11-04'),(9,2,'2019-01-24'),(9,2,'2019-05-15'),(9,2,'2020-02-27'),(9,2,'2020-08-03'),(10,3,'2020-05-24'),(10,1,'2021-01-11');
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
 
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
 
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
 
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
 
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
 
+INSERT INTO species (name) VALUES ('Pokemon');
+
+INSERT INTO species (name) VALUES ('Digimon');
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+
+UPDATE animals SET species_id = 1;
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+
+UPDATE animals SET owner_id = 2 WHERE name  IN ('Pikachu','Gabumon');
+
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon','Plantmon');
+
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander','Squirtle','Blossom');
+
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon','Boarmon');
+
+UPDATE owners SET full_name = 'Melody Pond' WHERE id = 4;
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, '04-23-2000');
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Maisy Smith', 26, '01-17-2019');
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Stephanie Mendez', 64, '05-17-1981');
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM public."animals ") animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
